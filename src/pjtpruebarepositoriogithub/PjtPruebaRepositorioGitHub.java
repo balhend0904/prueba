b8140885caf95dc904251f39c145629dadbd3fd3
balhe <balhe@DESKTOP-E0Q1UTT>
2024-03-19 18:27:43 -0600

    Mi aporte fue el msj Hola, mundo en el paquete pjtPrueba..

---
+++ src/pjtpruebarepositoriogithub/PjtPruebaRepositorioGitHub.java
@@ -0,0 +1,13 @@
+
+package pjtpruebarepositoriogithub;
+
+
+public class PjtPruebaRepositorioGitHub {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola, mundo!");
+    }
+    
+}
